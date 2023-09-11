6cbab992b69cce3af18994fbd95a9f3102f15cf7
Shafeeque E S <shafeeque.e.s@sap.com>
2023-09-11 15:57:34 +0530

    Remove duplicate imports
    Co-Authored-By: Ashish Ranjan Yadav <ashish.ranjan.yadav@sap.com>
    Co-Authored-By: Sonu Kumar Singh <sonu.kumar.singh02@sap.com>

--- pkg/utils/kubernetes/client/client_test.go
+++ pkg/utils/kubernetes/client/client_test.go
@@ -41,7 +41,6 @@ import (
 	"github.com/gardener/gardener/pkg/utils/flow"
 	kubernetesutils "github.com/gardener/gardener/pkg/utils/kubernetes"
 	. "github.com/gardener/gardener/pkg/utils/kubernetes/client"
-	utilclient "github.com/gardener/gardener/pkg/utils/kubernetes/client"
 	mockutilclient "github.com/gardener/gardener/pkg/utils/kubernetes/client/mock"
 	"github.com/gardener/gardener/pkg/utils/test"
 	mocktime "github.com/gardener/gardener/pkg/utils/time/mock"
@@ -356,10 +355,10 @@ var _ = Describe("Cleaner", func() {
 			cleaner = NewVolumeSnapshotContentCleaner(timeOps)
 			labels = map[string]string{"action": "cleanup"}
 			cleanOps = []CleanOption{
-				utilclient.ListWith{
+				ListWith{
 					client.MatchingLabels(labels),
 				},
-				utilclient.DeleteWith{
+				DeleteWith{
 					client.GracePeriodSeconds(29),
 				},
 			}
